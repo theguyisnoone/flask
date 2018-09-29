205f5d565aa51f781d1b58bede157dd771dc6d11
hajaking <953258481@qq.com>
2018-09-29 18:05:36 +0800

    laal

--- sqlalchemy/manage.py
+++ sqlalchemy/manage.py
@@ -1,5 +1,5 @@
 from flask_script import Manager,Server
-from main import app,db,User
+from main import app,db,User#
 
 manager=Manager(app)
 
@@ -7,7 +7,7 @@ manager.add_command("server",Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User)#
 
 
 
